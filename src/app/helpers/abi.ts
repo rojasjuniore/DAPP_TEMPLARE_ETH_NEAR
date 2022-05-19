4e58046d557a1c072754c751902cf957b5ee1df4
Jhonny <jhonnycgarcia@gmail.com>
2022-05-18 20:50:39 -0500

    UP - Se importa nuevo helper ´abi´.

---
+++ src/app/helpers/abi.ts
@@ -0,0 +1,51 @@
+export const approve = [{
+    "inputs": [
+        {
+            "internalType": "address",
+            "name": "tokenOwner",
+            "type": "address"
+        },
+        {
+            "internalType": "uint256",
+            "name": "amount",
+            "type": "uint256"
+        }
+    ],
+    "name": "approve",
+    "outputs": [
+        {
+            "internalType": "bool",
+            "name": "",
+            "type": "bool"
+        }
+    ],
+    "stateMutability": "nonpayable",
+    "type": "function"
+}];
+
+
+export const allowance = [{
+    "inputs": [
+        {
+            "internalType": "address",
+            "name": "owner",
+            "type": "address"
+        },
+        {
+            "internalType": "address",
+            "name": "spender",
+            "type": "address"
+        }
+    ],
+    "name": "allowance",
+    "outputs": [
+        {
+            "internalType": "uint256",
+            "name": "",
+            "type": "uint256"
+        }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+},];
+
